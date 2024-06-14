57e86cd0640b20de49d754c39f5ff6e340fd8dde
Nivas <kumaravelnivas@gmail.com>
2024-06-14 15:26:15 +0530

    Committing Main Dart

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  New_SSP_Plan1_Screen(),
+      home:  LoginScreen(),
     );
   }
 }
